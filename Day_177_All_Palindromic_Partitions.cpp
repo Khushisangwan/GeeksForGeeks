7908733c7ab39bd148a4b314b96ba481de36fc43
khushi <khushisangwan441@gmail.com>
2025-06-18 12:00:03 +0530

    Refactor palinParts function to improve structure and readability

--- Day_177_All_Palindromic_Partitions.cpp
+++ Day_177_All_Palindromic_Partitions.cpp
@@ -10,40 +10,43 @@ using namespace std;
 // Return all possible palindromic partitions of the string.
 
 class Solution {
-  public:
-  
-    vector<vector<string>> res;
+    public:
     
-    bool check(string &s){
-        int start = 0 , end = s.length()-1;
-        while(start <= end){
-            if(s[start] != s[end]) return false;
-            start++;
-            end--;
-        }
-        return true;
-    }
-    
-    void solve(string &s , int i , vector<string>&temp){
-        if(i >= s.length()){
-            res.push_back(temp);
-            return;
+        vector<vector<string>> res; // Stores all palindromic partitions
+        
+        // Function to check if a given string is a palindrome
+        bool check(string &s){
+                int start = 0 , end = s.length()-1;
+                while(start <= end){
+                        if(s[start] != s[end]) return false; // Not a palindrome
+                        start++;
+                        end--;
+                }
+                return true; // Is a palindrome
         }
         
-        string tmp;
-        for(int j=i;j<s.length();j++){
-            tmp.push_back(s[j]);
-            if(check(tmp)){
-                temp.push_back(tmp);
-                solve(s,j+1,temp);
-                temp.pop_back();
-            }
+        // Recursive function to find all palindromic partitions
+        void solve(string &s , int i , vector<string>&temp){
+                if(i >= s.length()){
+                        res.push_back(temp); // Found a valid partition
+                        return;
+                }
+                
+                string tmp;
+                for(int j=i;j<s.length();j++){
+                        tmp.push_back(s[j]); // Build substring s[i..j]
+                        if(check(tmp)){ // If substring is palindrome
+                                temp.push_back(tmp); // Choose
+                                solve(s,j+1,temp);   // Explore
+                                temp.pop_back();     // Un-choose (backtrack)
+                        }
+                }
         }
-    }
-  
-    vector<vector<string>> palinParts(string &s) {
-        vector<string> temp;
-        solve(s,0,temp);
-        return res;
-    }
+    
+        // Main function to return all palindromic partitions
+        vector<vector<string>> palinParts(string &s) {
+                vector<string> temp;
+                solve(s,0,temp);
+                return res;
+        }
 };
