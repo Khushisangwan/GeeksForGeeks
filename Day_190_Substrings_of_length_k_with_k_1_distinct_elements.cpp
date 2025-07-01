73d9a6cb6ca712897e779f63fffa737984661af1
khushi <khushisangwan441@gmail.com>
2025-07-01 23:18:57 +0530

    Refactor substrCount function for improved readability and maintainability by adding comments and clarifying the sliding window approach.

--- Day_190_Substrings_of_length_k_with_k_1_distinct_elements.cpp
+++ Day_190_Substrings_of_length_k_with_k_1_distinct_elements.cpp
@@ -4,23 +4,27 @@ using namespace std;
 // Approach: Sliding Window Technique
 // Steps:
 class Solution {
-  public:
-    int substrCount(string &s, int k) {
-        // code here
-         map<int,int> mp;
-        int i=0,j=0,ans=0;
-        while(j<s.size()){
-            mp[s[j]]++;
-            if(j-i+1>=k){
-                while(j-i+1>k){
-                    mp[s[i]]--;
-                    if(mp[s[i]]==0)mp.erase(s[i]);
-                    i++;
+    public:
+        int substrCount(string &s, int k) {
+                // Map to store frequency of characters in current window
+                map<int,int> mp;
+                int i=0, j=0, ans=0;
+                // Traverse the string with sliding window
+                while(j < s.size()){
+                        mp[s[j]]++; // Add current character to map
+                        // If window size is at least k
+                        if(j-i+1 >= k){
+                                // Shrink window if it exceeds size k
+                                while(j-i+1 > k){
+                                        mp[s[i]]--;
+                                        if(mp[s[i]] == 0) mp.erase(s[i]);
+                                        i++;
+                                }
+                                // If window has exactly k-1 distinct characters, increment answer
+                                if(mp.size() == k-1) ans++;
+                        }
+                        j++;
                 }
-                if(mp.size()==k-1)ans++;
-            }
-            j++;
+                return ans;
         }
-        return ans;
-    }
 };
