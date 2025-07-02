3a13bd85de5352a0512e214e7dcf5df2f26fd355
khushi <khushisangwan441@gmail.com>
2025-07-02 23:05:02 +0530

    Refactor totalElements function for improved readability by adding comments and clarifying the sliding window approach.

--- Day_191_Longest_subarray_with_Atmost_two_distinct_integers.cpp
+++ Day_191_Longest_subarray_with_Atmost_two_distinct_integers.cpp
@@ -18,24 +18,27 @@ using namespace std;
 class Solution {
   public:
      int totalElements(vector<int> &arr) {
-        // code here
-        int ans=0;
-        int i=0,j=0;
-        map<int,int> mp;
-        while(j<arr.size()){
-            mp[arr[j]]++;
-            int c=mp.size();
-            while(c>2){
-                mp[arr[i]]--;
-                if(mp[arr[i]]==0){
-                    mp.erase(arr[i]);
-                    c--;
-                }
-                i++;
+        int ans = 0; // To store the maximum length found
+        int i = 0, j = 0; // Window pointers
+        map<int, int> mp; // Map to store frequency of elements in the current window
+
+        while (j < arr.size()) {
+            mp[arr[j]]++; // Add current element to the map (expand window)
+            int c = mp.size(); // Number of distinct elements in the window
+
+            // If more than 2 distinct elements, shrink window from the left
+            while (c > 2) {
+            mp[arr[i]]--; // Decrease frequency of leftmost element
+            if (mp[arr[i]] == 0) {
+                mp.erase(arr[i]); // Remove element if its frequency is zero
+                c--; // Decrement count of distinct elements
             }
-            ans=max(ans,j-i+1);
-            j++;
+            i++; // Move left pointer to the right
+            }
+
+            ans = max(ans, j - i + 1); // Update maximum length if current window is valid
+            j++; // Move right pointer to the right
         }
-        return ans;
+        return ans; // Return the maximum length found
     }
 };
