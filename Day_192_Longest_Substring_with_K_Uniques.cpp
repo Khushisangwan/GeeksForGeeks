27be04204b501e3433cbc7ee1098ca907e090d0e
khushi <khushisangwan441@gmail.com>
2025-07-05 23:54:18 +0530

    Add problem statement and difficulty level comments for maxSum function

--- Day_192_Longest_Substring_with_K_Uniques.cpp
+++ Day_192_Longest_Substring_with_K_Uniques.cpp
@@ -40,6 +40,7 @@ class Solution {
             }
         }
         return maxAns;
+        
     }
     
 };
--- Day_194_Max_Score_from_Subarray_Mins.cpp
+++ Day_194_Max_Score_from_Subarray_Mins.cpp
@@ -1,3 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+// Problem: Max Score from Subarray Mins
+// Link: https://leetcode.com/problems/max-score-from-subarray-mins/
+// Difficulty: Medium
 class Solution {
   public:
     int maxSum(vector<int> &arr) {
