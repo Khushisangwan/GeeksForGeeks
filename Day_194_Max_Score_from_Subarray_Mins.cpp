e60c8739be113cba39ccabdb6d7e7887fce029a7
khushi <khushisangwan441@gmail.com>
2025-07-05 23:54:55 +0530

    Refactor formatting and add initial commit comment for maxSum method in Solution class

--- Day_194_Max_Score_from_Subarray_Mins.cpp
+++ Day_194_Max_Score_from_Subarray_Mins.cpp
@@ -3,13 +3,14 @@ using namespace std;
 // Problem: Max Score from Subarray Mins
 // Link: https://leetcode.com/problems/max-score-from-subarray-mins/
 // Difficulty: Medium
+// Commit 1: Implement initial Solution class with maxSum method
 class Solution {
-  public:
-    int maxSum(vector<int> &arr) {
-        int count=0;
-        for(int i=0;i<arr.size()-1;i++)
-            count=max(count,arr[i]+arr[i+1]);
-        
-        return count;
-    }
+    public:
+        int maxSum(vector<int> &arr) {
+                int count=0;
+                for(int i=0;i<arr.size()-1;i++)
+                        count=max(count,arr[i]+arr[i+1]);
+                
+                return count;
+        }
 };
