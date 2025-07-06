8ec2e88d9ac7a01bc7548138cf6b5361fef9873f
khushi <khushisangwan441@gmail.com>
2025-07-06 23:20:49 +0530

    Refactor topKSumPairs function to improve code readability and maintainability by adding comments and organizing logic

--- Day_195_Maximum_Sum_Combination.cpp
+++ Day_195_Maximum_Sum_Combination.cpp
@@ -3,31 +3,37 @@ using namespace std;
 // Problem: Maximum Sum Combination
 // Link: https://practice.geeksforgeeks.org/problems/maximum-sum-combination/
 class Solution {
-  public:
-    vector<int> topKSumPairs(vector<int>& a, vector<int>& b, int k) {
-        // code here
-        int n=a.size(), m=b.size();
-        sort(a.begin(), a.end(), greater<int>());
-        sort(b.begin(), b.end(), greater<int>());
-        
-        priority_queue<int, vector<int>, greater<int>>pq;
-        
-        for(int i=0; i<k; i++){
-            for(int j=0; j<k; j++){
-                int sum=a[i]+b[j];
-                if(pq.size()==k && sum<pq.top()) break;
-                pq.push(a[i]+b[j]);
-                if(pq.size()>k) pq.pop();
-            }
+    public:
+        vector<int> topKSumPairs(vector<int>& a, vector<int>& b, int k) {
+                // Sort both arrays in descending order to get largest elements first
+                int n = a.size(), m = b.size();
+                sort(a.begin(), a.end(), greater<int>());
+                sort(b.begin(), b.end(), greater<int>());
+                
+                // Min-heap to keep track of top k sums
+                priority_queue<int, vector<int>, greater<int>> pq;
+                
+                // Only consider the first k elements from both arrays
+                for (int i = 0; i < k; i++) {
+                        for (int j = 0; j < k; j++) {
+                                int sum = a[i] + b[j];
+                                // If heap is full and current sum is less than the smallest in heap, break inner loop
+                                if (pq.size() == k && sum < pq.top()) break;
+                                pq.push(sum);
+                                // Maintain heap size to k
+                                if (pq.size() > k) pq.pop();
+                        }
+                }
+                
+                // Extract results from heap into a vector
+                vector<int> ans;
+                while (!pq.empty()) {
+                        ans.push_back(pq.top());
+                        pq.pop();
+                }
+                // Reverse to get results in descending order
+                reverse(ans.begin(), ans.end());
+                
+                return ans;
         }
-        
-        vector<int>ans;
-        while(!pq.empty()){
-            ans.push_back(pq.top());
-            pq.pop();
-        }
-        reverse(ans.begin(), ans.end());
-        
-        return ans;
-    }
 };
