ba48f5b05945665055ac4c89b60140c20f2f7e89
khushi <khushisangwan441@gmail.com>
2025-08-14 23:51:41 +0530

    Add optimized egg dropping solution combining iterative halving and binary search to minimize trials

--- Egg_Dropping_Puzzel.cpp
+++ Egg_Dropping_Puzzel.cpp
@@ -1,3 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Problem: Egg Dropping Puzzle
+// Approach: Use a combination of iterative halving and binary search to minimize the number of trials needed to find the critical floor
+
+// Steps:
+// 1. If there is only one egg, the number of trials is equal to the number of floors.
+// 2. If there are two eggs, we can use a halving strategy to reduce the number of trials.
+// 3. For more than two eggs, we can use binary search to find the minimum number of trials needed.
+// 4. The idea is to find the maximum number of floors that can be tested with a given number of trials and eggs.
+// 5. We can calculate the number of floors that can be tested with `x` trials and `n` eggs using the formula:
+//    F(x, n) = F(x-1, n-1) + F(x-1, n) + 1
+//    where F(x, n) is the maximum number of floors that can be tested with `x` trials and `n` eggs.
+// 6. We can use binary search to find the minimum number of trials needed to test `k` floors with `n` eggs.
+
+// Time Complexity: O(log k) for the halving and O(sqrt(k)) for the binary search, leading to an overall complexity of O(sqrt(k) * log k)
+// Space Complexity: O(1) as we are using a constant amount of space
+
 
 class Solution {
   public:
