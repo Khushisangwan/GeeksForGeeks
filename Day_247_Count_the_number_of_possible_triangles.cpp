c3b3087ffbc1e8dd0c9c7dfb93370cee3083f8c4
khushi <khushisangwan441@gmail.com>
2025-08-27 23:32:08 +0530

    style: improve formatting and organization of countTriangles function

--- Day_247_Count_the_number_of_possible_triangles.cpp
+++ Day_247_Count_the_number_of_possible_triangles.cpp
@@ -2,28 +2,34 @@
 using namespace std;
 
 class Solution {
-  public:
-    // Function to count the number of possible triangles.
-    int countTriangles(vector<int>& arr) {
-        int ans = 0;
-        sort(arr.begin(), arr.end());
-        
-        for(int i=2; i<arr.size(); i++){
-            int left = 0, right = i - 1;
-            
-            while(left < right){
-                if(arr[left] + arr[right] > arr[i]){
-                    ans += right - left;
-                    right--;
+    public:
+        // Function to count the number of possible triangles.
+        int countTriangles(vector<int>& arr) {
+                int ans = 0;
+                // Sort the array to use two-pointer technique
+                sort(arr.begin(), arr.end());
+                
+                // Fix the largest side of triangle (at index i)
+                for(int i=2; i<arr.size(); i++){
+                        int left = 0, right = i - 1;
+                        
+                        // Use two pointers to find valid triangles
+                        while(left < right){
+                                // If sum of two smaller sides > largest side, triangle is valid
+                                if(arr[left] + arr[right] > arr[i]){
+                                        // All elements from left to right-1 can form triangles with arr[right] and arr[i]
+                                        ans += right - left;
+                                        right--; // Move right pointer to check smaller values
+                                }
+                                else{
+                                        // Sum is too small, move left pointer to get larger sum
+                                        left++;
+                                }
+                        }
                 }
-                else{
-                    left++;
-                }
-            }
+                
+                return ans;
         }
         
-        return ans;
-    }
-    
 };
 
