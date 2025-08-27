860bf01dd1d72d33ea31ff526041210fd62cdbac
khushi <khushisangwan441@gmail.com>
2025-08-27 23:30:45 +0530

    feat: implement countTriangles function to count the number of possible triangles

---
+++ Day_247_Count_the_number_of_possible_triangles.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    // Function to count the number of possible triangles.
+    int countTriangles(vector<int>& arr) {
+        int ans = 0;
+        sort(arr.begin(), arr.end());
+        
+        for(int i=2; i<arr.size(); i++){
+            int left = 0, right = i - 1;
+            
+            while(left < right){
+                if(arr[left] + arr[right] > arr[i]){
+                    ans += right - left;
+                    right--;
+                }
+                else{
+                    left++;
+                }
+            }
+        }
+        
+        return ans;
+    }
+    
+};
+
