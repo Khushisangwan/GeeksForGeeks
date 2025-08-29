d1990297137f4a8e8a71a47b706f816a649c02dc
khushi <khushisangwan441@gmail.com>
2025-08-29 22:49:24 +0530

    style: improve formatting and organization of maxOnes function for clarity

--- Day_248_Maximize_Number_of_1_s.cpp
+++ Day_248_Maximize_Number_of_1_s.cpp
@@ -1,19 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 class Solution {
-  public:
-    int maxOnes(vector<int>& arr, int k) {
-        int n=arr.size();
-        int i=0,j=0,count=0,maxi=0;
-        while(j<n){
-            if(arr[j]==0) count++;
-            while(count>k){
-                if(arr[i]==0) count--;
-                i++;
-            }
-            maxi=max(maxi,j-i+1);
-            j++;
+    public:
+        int maxOnes(vector<int>& arr, int k) {
+                int n = arr.size();
+                int i = 0, j = 0, count = 0, maxi = 0;  // i: left pointer, j: right pointer, count: zeros in window, maxi: max window size
+                
+                while(j < n) {
+                        // If current element is 0, increment zero count
+                        if(arr[j] == 0) count++;
+                        
+                        // Shrink window from left if zeros exceed k
+                        while(count > k) {
+                                if(arr[i] == 0) count--;  // Decrease zero count when removing a zero from window
+                                i++;  // Move left pointer
+                        }
+                        
+                        // Update maximum window size
+                        maxi = max(maxi, j - i + 1);
+                        j++;  // Expand window to the right
+                }
+                
+                return maxi;  // Return maximum number of 1s possible
         }
-        return maxi;
-    }
 };
