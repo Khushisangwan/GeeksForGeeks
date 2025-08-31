417383970277b7c8d6ba081f7b916489d21a46c8
khushi <khushisangwan441@gmail.com>
2025-08-31 22:57:16 +0530

    refactor: enhance maxWater function with detailed comments and improved clarity

--- Day_251_Container_With_Most_Water.cpp
+++ Day_251_Container_With_Most_Water.cpp
@@ -1,18 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 class Solution {
-  public:
-    int maxWater(vector<int> &arr) {
-        // code here
-        int l = 0;
-        int h = arr.size() - 1;
-        
-        int mx = 0;
-        while(l <= h){
-            mx = max(mx, (h - l) * min(arr[l], arr[h]));
-            if(arr[l] < arr[h]) l++;
-            else h--;
+    public:
+        int maxWater(vector<int> &arr) {
+                // Two pointer approach to find maximum water container
+                int l = 0;                    // Left pointer starting from beginning
+                int h = arr.size() - 1;       // Right pointer starting from end
+                
+                int mx = 0;                   // Variable to store maximum water area
+                
+                // Continue until pointers meet
+                while(l <= h){
+                        // Calculate area: width * height (min of two heights)
+                        mx = max(mx, (h - l) * min(arr[l], arr[h]));
+                        
+                        // Move pointer with smaller height inward
+                        // This gives chance to find potentially larger area
+                        if(arr[l] < arr[h]) 
+                                l++;                  // Move left pointer right
+                        else 
+                                h--;                  // Move right pointer left
+                }
+                return mx;                    // Return maximum water area found
         }
-        return mx;
-    }
 };
