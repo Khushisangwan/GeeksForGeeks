695e046837761b7a33d3644ca4bba282370142fe
khushi <khushisangwan441@gmail.com>
2025-09-02 21:01:31 +0530

    refactor: enhance readability and structure of swapKth function

--- Day_253_Swap_Kth_nodes_from_ends.cpp
+++ Day_253_Swap_Kth_nodes_from_ends.cpp
@@ -13,36 +13,44 @@ class Node {
 #include<bits/stdc++.h>
 using namespace std;
 class Solution {
-  public:
-    Node* swapKth(Node* head, int k) {
-        // code here
-         Node* start=head;
-        int len=0;
-        while(start){
-            len++;
-            start=start->next;
+    public:
+        Node* swapKth(Node* head, int k) {
+                // Find the length of the linked list
+                Node* start = head;
+                int len = 0;
+                while(start) {
+                        len++;
+                        start = start->next;
+                }
+                
+                // If k is greater than length, no swap needed
+                if(k > len) return head;
+                
+                // Calculate positions: kth from start and kth from end
+                int s = k;              // kth node from start
+                int e = len - k + 1;    // kth node from end
+                
+                // If both positions are same, no swap needed
+                if(e == s) return head;
+                
+                // Find the nodes at positions s and e
+                start = head;
+                int idx = 1;
+                Node* point1;  // kth node from start
+                Node* point2;  // kth node from end
+                
+                while(start) {
+                        if(idx == s) point1 = start;
+                        else if(idx == e) point2 = start;
+                        start = start->next;
+                        idx++;
+                }
+                
+                // Swap the data of the two nodes
+                int temp = point1->data;
+                point1->data = point2->data;
+                point2->data = temp;
+                
+                return head;
         }
-        
-        if(k>len)return head;
-        int s=k;
-        int e=len-k+1;
-        if(e==s)return head;
-        
-        start=head;
-        int idx=1;
-        Node* point1;
-        Node* point2;
-        while(start){
-            if(idx==s)point1=start;
-            else if(idx==e)point2=start;
-            start=start->next;
-            idx++;
-        }
-        
-        int temp=point1->data;
-        point1->data=point2->data;
-        point2->data=temp;
-        return head;
-    
-    }
 };
