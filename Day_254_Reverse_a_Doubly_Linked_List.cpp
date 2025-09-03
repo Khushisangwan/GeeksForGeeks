90c6d14f15a0fb4279754148d8a828d52736c5a1
khushi <khushisangwan441@gmail.com>
2025-09-03 23:14:22 +0530

    refactor: improve readability and structure of reverse function in doubly linked list

--- Day_254_Reverse_a_Doubly_Linked_List.cpp
+++ Day_254_Reverse_a_Doubly_Linked_List.cpp
@@ -15,17 +15,25 @@ class Node {
 #include <bits/stdc++.h>
 using namespace std;
 class Solution {
-  public:
-    Node *reverse(Node *head) {
-        Node * temp=head;
-        while (temp){
-            Node * node = temp->next;
-            temp->next = temp->prev;
-            temp->prev = node;
-            if(!node) break;
-            temp = node;
+    public:
+        Node *reverse(Node *head) {
+                Node * temp = head;  // Start from the head of the list
+                
+                // Traverse through the entire doubly linked list
+                while (temp) {
+                        Node * node = temp->next;  // Store the next node before modifying pointers
+                        
+                        // Swap the next and prev pointers of current node
+                        temp->next = temp->prev;
+                        temp->prev = node;
+                        
+                        // If we've reached the end of the original list, break
+                        if (!node) break;
+                        
+                        temp = node;  // Move to the next node in the original list
+                }
+                
+                // temp now points to the new head (originally the tail)
+                return temp;
         }
-        
-        return temp;
-    }
 };
