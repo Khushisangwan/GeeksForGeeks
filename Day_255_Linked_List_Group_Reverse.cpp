3adeb128fc5a682bd6b58068fff1536491e059d8
khushi <khushisangwan441@gmail.com>
2025-09-04 23:13:38 +0530

    refactor: enhance readability of reverseKGroup function by adding comments and improving structure

--- Day_255_Linked_List_Group_Reverse.cpp
+++ Day_255_Linked_List_Group_Reverse.cpp
@@ -1,31 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 class Solution {
-  public:
-    Node *reverseKGroup(Node *head, int k) {
-        // code here
-        Node*temp=head;
-        Node*temp2=head;
-        
-        while(temp!=nullptr){
-                stack<int>st;
-            for(int i=1;i<=k;i++){
-                if(temp==nullptr){
-                    break;
+    public:
+        Node *reverseKGroup(Node *head, int k) {
+                // Function to reverse nodes in groups of k
+                
+                Node*temp=head;   // Pointer to traverse the list
+                Node*temp2=head;  // Pointer to update node values
+                
+                // Traverse the entire linked list
+                while(temp!=nullptr){
+                        stack<int>st;  // Stack to store k node values for reversal
+                        
+                        // Push k node values into stack (or until end of list)
+                        for(int i=1;i<=k;i++){
+                                if(temp==nullptr){
+                                        break;  // Break if we reach end before k nodes
+                                }
+                                st.push(temp->data);  // Push current node's data
+                                temp=temp->next;      // Move to next node
+                        }
+                        
+                        // Pop from stack and update nodes (reverses the order)
+                        while(!st.empty()){
+                                int val=st.top();    // Get top value from stack
+                                st.pop();            // Remove top element
+                                temp2->data=val;     // Update current node with reversed value
+                                temp2=temp2->next;   // Move to next node to update
+                        }
                 }
-                st.push(temp->data);
-                temp=temp->next;
-            }
-            
-            while(!st.empty()){
-                int val=st.top();
-                st.pop();
-                temp2->data=val;
-                temp2=temp2->next;
-            }
-            //temp=temp->next;
+                return head;  // Return the modified list head
         }
-        return head;
-    }
 };
-
