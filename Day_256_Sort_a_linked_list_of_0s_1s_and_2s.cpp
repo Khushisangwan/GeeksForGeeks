e63b630593549e2453b4137e2db73f9e74127688
khushi <khushisangwan441@gmail.com>
2025-09-05 23:44:30 +0530

    refactor: improve code readability in segregate function by adding comments and organizing logic

--- Day_256_Sort_a_linked_list_of_0s_1s_and_2s.cpp
+++ Day_256_Sort_a_linked_list_of_0s_1s_and_2s.cpp
@@ -11,36 +11,46 @@
 }*/
 #include <bits/stdc++.h>
 using namespace std;
+
 class Solution {
     static Node segregate(Node head) {
         // code here
-         int zero = 0, one = 0, two = 0;
+        
+        // Count occurrences of 0s, 1s, and 2s
+        int zero = 0, one = 0, two = 0;
         Node curr = head;
-        while(curr!=null){
-            if(curr.data==0){
+        
+        // First pass: count the frequency of each value
+        while(curr != null) {
+            if(curr.data == 0) {
                 zero++;
             }
-            else if(curr.data==1){
+            else if(curr.data == 1) {
                 one++;
             }
-            else{
+            else {
                 two++;
             }
             curr = curr.next;
         }
         
+        // Reset current pointer to head for second pass
         curr = head;
         
-        while(curr!=null){
-            if(zero!=0){
+        // Second pass: overwrite data with sorted values
+        while(curr != null) {
+            // Fill with 0s first
+            if(zero != 0) {
                 curr.data = 0;
                 zero--;
             }
-            else if(one!=0){
+            // Then fill with 1s
+            else if(one != 0) {
                 curr.data = 1;
                 one--;
             }
-            else if(two!=0){
+            // Finally fill with 2s
+            else if(two != 0) {
                 curr.data = 2;
                 two--;
             }
