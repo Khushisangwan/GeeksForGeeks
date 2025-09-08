2ddafa9baf30130725263a850e4adcfc451d2e6a
khushi <khushisangwan441@gmail.com>
2025-09-08 22:51:47 +0530

    refactor: enhance readability and organization in mergeSort function

--- Day_259_Merge_Sort_for_Linked_List.cpp
+++ Day_259_Merge_Sort_for_Linked_List.cpp
@@ -16,25 +16,38 @@ struct Node {
 using namespace std;
 
 class Solution {
-  public:
-    // Function to sort the given linked list using Merge Sort.
-    Node* mergeSort(Node* head) {
-        Node *temp = head;
-        vector<int> v;
-        while(temp!=NULL){
-            v.push_back(temp->data);
-            temp = temp->next;
+    public:
+        // Function to sort the given linked list using Merge Sort.
+        Node* mergeSort(Node* head) {
+                // Initialize a temporary pointer to traverse the linked list
+                Node *temp = head;
+                
+                // Vector to store all the data values from the linked list
+                vector<int> v;
+                
+                // Traverse the linked list and copy all data values to the vector
+                while(temp != NULL){
+                        v.push_back(temp->data);
+                        temp = temp->next;
+                }
+                
+                // Sort the vector using STL sort function
+                sort(v.begin(), v.end());
+                
+                // Reset temp pointer to head for second traversal
+                temp = head;
+                int i = 0;
+                
+                // Traverse the linked list again and update node values with sorted data
+                while(temp != NULL && i < v.size()){
+                        temp->data = v[i];
+                        temp = temp->next;
+                        i++;
+                }
+                
+                // Return the head of the modified linked list
+                return head;
         }
-        sort(v.begin(),v.end());
-        temp = head;
-        int i = 0;
-        while(temp!=NULL && i<v.size()){
-            temp->data = v[i];
-            temp = temp->next;
-            i++;
-        }
-        return head;
-    }
 };
 
 
