1ce228003e2f345028cff7a548401161f3c014fb
khushi <khushisangwan441@gmail.com>
2025-09-08 22:50:42 +0530

    feat: implement merge sort for linked list with Node structure

---
+++ Day_259_Merge_Sort_for_Linked_List.cpp
@@ -0,0 +1,38 @@
+
+/*
+The structure of linked list is the following
+
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    // Function to sort the given linked list using Merge Sort.
+    Node* mergeSort(Node* head) {
+        Node *temp = head;
+        vector<int> v;
+        while(temp!=NULL){
+            v.push_back(temp->data);
+            temp = temp->next;
+        }
+        sort(v.begin(),v.end());
+        temp = head;
+        int i = 0;
+        while(temp!=NULL && i<v.size()){
+            temp->data = v[i];
+            temp = temp->next;
+            i++;
+        }
+        return head;
+    }
+};
+
+
