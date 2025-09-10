b9e30c23144078a69401026822c6cc99d0ada0d7
khushi <khushisangwan441@gmail.com>
2025-09-10 23:41:34 +0530

    refactor: enhance readability and comments in largestSwap function

--- Day_261_Largest_number_in_one_swap.cpp
+++ Day_261_Largest_number_in_one_swap.cpp
@@ -1,29 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 class Solution {
-  public:
-    string largestSwap(string &s) {
-        // code here
-          int n=s.size();
-        int ind=0;
-        string t=s;
-        sort(t.begin(), t.end(),greater<int>());
-        int d =-1;
-        for(int i=0;i<n;i++){
-            if(t[i]!=s[i])
-            {
-                d=i;
-                break;
-            }
+    public:
+        string largestSwap(string &s) {
+                // Get the length of the string
+                int n = s.size();
+                
+                // Create a copy of the string and sort it in descending order
+                // This gives us the largest possible arrangement
+                string t = s;
+                sort(t.begin(), t.end(), greater<int>());
+                
+                // Find the first position where original string differs from sorted string
+                int d = -1;
+                for(int i = 0; i < n; i++){
+                        if(t[i] != s[i]) {
+                                d = i;
+                                break;
+                        }
+                }
+                
+                // If no difference found, string is already in largest form
+                if (d == -1) return s;
+                
+                // Find the rightmost occurrence of the target digit to maximize the swap
+                // We search from right to left to get the digit at the rightmost position
+                for(int i = n - 1; i >= 0; i--){
+                        if(t[d] == s[i]){
+                                // Swap the digits to get the largest number
+                                swap(s[d], s[i]);
+                                break;
+                        }
+                }
+                
+                return s;
         }
-        if (d==-1) return s;
-        for(int i=n-1;i>=0;i--){
-            if(t[d]==s[i]){
-                swap(s[d],s[i]);
-                break;
-            }
-        }
-        
-        return s;
-    }
 };
