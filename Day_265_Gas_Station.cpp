0a0cd077f4c53838cb3861709a861b669330b672
khushi <khushisangwan441@gmail.com>
2025-09-14 23:54:57 +0530

    refactor: improve readability and structure of startStation function

--- Day_265_Gas_Station.cpp
+++ Day_265_Gas_Station.cpp
@@ -1,28 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
 class Solution {
-  public:
-    int startStation(vector<int> &gas, vector<int> &cost) {
-        //  code here
-          int n = gas.size();
-        int totalGas = 0, totalCost = 0;
-        
-        for (int i = 0; i < n; i++) {
-            totalGas += gas[i];
-            totalCost += cost[i];
+    public:
+        int startStation(vector<int> &gas, vector<int> &cost) {
+                // Get the number of gas stations
+                int n = gas.size();
+                int totalGas = 0, totalCost = 0;
+                
+                // Calculate total gas available and total cost required
+                for (int i = 0; i < n; i++) {
+                        totalGas += gas[i];
+                        totalCost += cost[i];
+                }
+                
+                // If total gas is less than total cost, it's impossible to complete the circuit
+                if (totalGas < totalCost) return -1;
+                
+                // Initialize starting station and current tank fuel
+                int start = 0, tank = 0;
+                
+                // Find the optimal starting station
+                for (int i = 0; i < n; i++) {
+                        // Add net fuel (gas gained minus cost to travel)
+                        tank += gas[i] - cost[i];
+                        
+                        // If tank goes negative, we can't reach next station from current start
+                        // Reset starting point to next station and reset tank
+                        if (tank < 0) {
+                                start = i + 1;
+                                tank = 0;
+                        }
+                }
+                
+                // Return the starting station index
+                return start;
         }
-        
-        // If total gas is less than total cost, it's impossible
-        if (totalGas < totalCost) return -1;
-        
-        int start = 0, tank = 0;
-        for (int i = 0; i < n; i++) {
-            tank += gas[i] - cost[i];
-            
-            // If tank goes negative, reset starting point
-            if (tank < 0) {
-                start = i + 1;
-                tank = 0;
-            }
-        }
-        return start;
-    }
 };
