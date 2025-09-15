603ba1fa04b1e03a3f2ad1f4fc4a4c08d3cb238f
khushi <khushisangwan441@gmail.com>
2025-09-15 21:17:43 +0530

    refactor: enhance readability and structure of stringStack function

--- Day_266_String_stack.cpp
+++ Day_266_String_stack.cpp
@@ -1,40 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 class Solution {
-  public:
-    bool stringStack(string &pat, string &tar) {
-        // code here
-         int n= pat.size();
-              int m= tar.size();
-              
-              int i=n-1;
-              int j=m-1;
-              int count =0;
-              while (i>=0 && j>=0){
-                     
-                      if (pat[i]==tar[j]){
-                            if (count%2==0) {
-                                j--;
-                                count=0;
-                                i--;
-                            }
-                            else{
-                                 i--;
-                                 count++;
-                         
-                            }
-                            
-                      }
-                          
-                      else{
-                           i--; 
-                           count++;
-                      }
-              }
-              if (j<0) return true;
-              else return false;
-              
-              
-              
-    }
+    public:
+        bool stringStack(string &pat, string &tar) {
+                // code here
+                int n = pat.size();  // Length of pattern string
+                int m = tar.size();  // Length of target string
+                
+                int i = n-1;  // Pointer for pattern string (starting from end)
+                int j = m-1;  // Pointer for target string (starting from end)
+                int count = 0;  // Counter to track operations
+                
+                // Traverse both strings from end to beginning
+                while (i >= 0 && j >= 0) {
+                        
+                        // If characters match
+                        if (pat[i] == tar[j]) {
+                                if (count % 2 == 0) {  // Even count means we can match directly
+                                        j--;  // Move target pointer backward
+                                        count = 0;  // Reset count
+                                        i--;  // Move pattern pointer backward
+                                }
+                                else {  // Odd count means we need to pop from stack
+                                        i--;  // Move pattern pointer backward
+                                        count++;  // Increment count
+                                }
+                        }
+                        else {  // Characters don't match
+                                i--;  // Move pattern pointer backward (skip this character)
+                                count++;  // Increment count (simulate pushing to stack)
+                        }
+                }
+                
+                // Return true if all target characters are matched
+                if (j < 0) return true;
+                else return false;
+        }
 };
