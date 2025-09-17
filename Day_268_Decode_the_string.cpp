faf8605bc0beb2574d41f133d2b1b9f54a99836e
khushi <khushisangwan441@gmail.com>
2025-09-17 22:56:29 +0530

    refactor: enhance readability and structure of decodedString function

--- Day_268_Decode_the_string.cpp
+++ Day_268_Decode_the_string.cpp
@@ -1,51 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
 class Solution {
-  public:
-    string decodedString(string &s) {
-        // code here
-          stack<string>num;
-        string res="";
-        int c=0;
-        for(char ch:s )
-        {
-            if(ch==']')
-            {
-                string str="";
-                while(num.top()!="[")
-                {
-                   str=num.top()+str;
-                   num.pop();
-                }
-                num.pop();
-                res="";
-                res=str+res;
-                str=res;
-                string dig="";
-                while(!num.empty() && isdigit(num.top()[0]))
-                {
-                    dig=num.top()+dig;
-                    num.pop();
-                }
-                long long n= stoll(dig);
-                while(--n > 0)
-                {
-                     res+=str;
+    public:
+        string decodedString(string &s) {
+                // Stack to store characters and brackets
+                stack<string> num;
+                string res = "";
+                int c = 0;
+                
+                // Process each character in the input string
+                for(char ch : s) {
+                        if(ch == ']') {
+                                // Found closing bracket, start decoding
+                                string str = "";
+                                
+                                // Extract the string inside brackets
+                                while(num.top() != "[") {
+                                        str = num.top() + str;
+                                        num.pop();
+                                }
+                                num.pop(); // Remove the opening bracket '['
+                                
+                                res = "";
+                                res = str + res;
+                                str = res;
+                                
+                                // Extract the number before the brackets
+                                string dig = "";
+                                while(!num.empty() && isdigit(num.top()[0])) {
+                                        dig = num.top() + dig;
+                                        num.pop();
+                                }
+                                
+                                // Convert digit string to number
+                                long long n = stoll(dig);
+                                
+                                // Repeat the string n times
+                                while(--n > 0) {
+                                        res += str;
+                                }
+                                
+                                // Push the decoded result back to stack
+                                num.push(res);
+                        } else {
+                                // Push current character to stack
+                                num.push(string() + ch);
+                        }
                 }
                 
-                num.push(res);
-            }else
-            {
-                num.push(string()+ch);
-            }
+                // Remove the last element and build final result
+                num.pop();
+                while(!num.empty()) {
+                        res = num.top() + res;
+                        num.pop();
+                }
+                
+                return res;
         }
-        num.pop();
-        while(!num.empty())
-        {
-            res=num.top()+res;
-            num.pop();
-        }
-        return res;
-    
-    }
 };
