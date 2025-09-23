bd9d5a36a5b3aace4d7374d093b9217cf636ff01
khushi <khushisangwan441@gmail.com>
2025-09-23 20:31:58 +0530

    refactor: enhance readability and structure of reverseQueue function

--- Day_274_Queue_Reversal.cpp
+++ Day_274_Queue_Reversal.cpp
@@ -3,25 +3,27 @@ using namespace std;
 // function Template for C++
 
 class Solution {
-  public:
-    queue<int> reverseQueue(queue<int> &q) {
-        // code here.
-        stack<int> st;
-    
-        while(!q.empty())
-        {
-            st.push(q.front());
-            q.pop();
-        }
+    public:
+        queue<int> reverseQueue(queue<int> &q) {
+                // Create a stack to store queue elements
+                stack<int> st;
         
-        while(!st.empty())
-        {
-            q.push(st.top());
-            st.pop();
+                // Transfer all elements from queue to stack
+                // This reverses the order due to LIFO nature of stack
+                while(!q.empty())
+                {
+                        st.push(q.front());  // Push front element of queue to stack
+                        q.pop();             // Remove front element from queue
+                }
+                
+                // Transfer all elements back from stack to queue
+                // Elements are now in reverse order
+                while(!st.empty())
+                {
+                        q.push(st.top());    // Push top element of stack to queue
+                        st.pop();            // Remove top element from stack
+                }
+                
+                return q;  // Return the reversed queue
         }
-        
-        return q;
-    }
 };
-
-
