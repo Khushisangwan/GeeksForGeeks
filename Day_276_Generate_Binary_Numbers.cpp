53aef0cc98d7aeafd49413bd6a7ce28e47f28d14
khushi <khushisangwan441@gmail.com>
2025-09-25 23:06:37 +0530

    refactor: improve comments and structure in generateBinary function

--- Day_276_Generate_Binary_Numbers.cpp
+++ Day_276_Generate_Binary_Numbers.cpp
@@ -1,15 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 class Solution {
-  public:
-    vector<string> generateBinary(int n) {
-        // code here
-           vector<string> v={"1"};
-        for(int i=1,j=0,x=0;i<n;i++){
-            v.push_back(v[j] + to_string(x));
-            x^=1;
-            if(x==0) j++;
+    public:
+        vector<string> generateBinary(int n) {
+                // Initialize result vector with first binary number "1"
+                vector<string> v={"1"};
+                
+                // Generate remaining n-1 binary numbers
+                for(int i=1,j=0,x=0;i<n;i++){
+                        // Append current bit (0 or 1) to the j-th generated number
+                        v.push_back(v[j] + to_string(x));
+                        
+                        // Toggle bit between 0 and 1
+                        x^=1;
+                        
+                        // Move to next base number when we've added both 0 and 1
+                        if(x==0) j++;
+                }
+                return v;
         }
-        return v;
-    }
 };
