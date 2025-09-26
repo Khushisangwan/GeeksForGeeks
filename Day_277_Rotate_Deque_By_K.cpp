900bcfc33b6913f6ce3892a61e611f04a9cccc27
khushi <khushisangwan441@gmail.com>
2025-09-26 23:48:37 +0530

    refactor: enhance comments for clarity in rotateDeque function

--- Day_277_Rotate_Deque_By_K.cpp
+++ Day_277_Rotate_Deque_By_K.cpp
@@ -2,18 +2,22 @@
 using namespace std;
 class Solution {
 public:
+    // Function to rotate a deque by k positions
+    // type = 1: rotate right (clockwise), type = 0: rotate left (counter-clockwise)
     static void rotateDeque(deque<int>& dq, int type, int k) {
         if (type == 1) {
             // Rotate right (last to front)
+            // Move elements from back to front k times
             while (k--) {
-                dq.push_front(dq.back());
-                dq.pop_back();
+                dq.push_front(dq.back()); // Take last element and add to front
+                dq.pop_back();             // Remove the last element
             }
         } else {
             // Rotate left (first to back)
+            // Move elements from front to back k times
             while (k--) {
-                dq.push_back(dq.front());
-                dq.pop_front();
+                dq.push_back(dq.front()); // Take first element and add to back
+                dq.pop_front();            // Remove the first element
             }
         }
     }
