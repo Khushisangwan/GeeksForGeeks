2b14e23307e823a6ea4dce507b88a09cd0618526
khushi <khushisangwan441@gmail.com>
2025-10-05 22:07:49 +0530

    refactor: reorganize solve function for improved readability and structure

--- Day_286_Rat_in_a_Maze.cpp
+++ Day_286_Rat_in_a_Maze.cpp
@@ -3,43 +3,53 @@
 using namespace std;
 
 class Solution {
-  public:
-    void solve(int i, int j, vector<vector<int>> &maze, int n, vector<string> &ans, string path, vector<vector<int>> &vis) {
-        if(i == n - 1 && j == n - 1) {
-            ans.push_back(path);
-            return;
+    public:
+        // Recursive function to find all paths from current position to destination
+        void solve(int i, int j, vector<vector<int>> &maze, int n, vector<string> &ans, string path, vector<vector<int>> &vis) {
+                // Base case: reached the destination (bottom-right corner)
+                if(i == n - 1 && j == n - 1) {
+                        ans.push_back(path);
+                        return;
+                }
+                
+                // Mark current cell as visited
+                vis[i][j] = 1;
+                
+                // Move Down: check if valid position and not visited
+                if(i + 1 < n && !vis[i + 1][j] && maze[i + 1][j] == 1) {
+                        solve(i + 1, j, maze, n, ans, path + "D", vis);
+                }
+                
+                // Move Left: check if valid position and not visited
+                if(j - 1 >= 0 && !vis[i][j - 1] && maze[i][j - 1] == 1) {
+                        solve(i, j - 1, maze, n, ans, path + "L", vis);
+                }
+                
+                // Move Right: check if valid position and not visited
+                if(j + 1 < n && !vis[i][j + 1] && maze[i][j + 1] == 1) {
+                        solve(i, j + 1, maze, n, ans, path + "R", vis);
+                }
+                
+                // Move Up: check if valid position and not visited
+                if(i - 1 >= 0 && !vis[i - 1][j] && maze[i - 1][j] == 1) {
+                        solve(i - 1, j, maze, n, ans, path + "U", vis);
+                }
+                
+                // Backtrack: unmark current cell as visited
+                vis[i][j] = 0;
         }
         
-        vis[i][j] = 1;
-        
-        if(i + 1 < n && !vis[i + 1][j] && maze[i + 1][j] == 1) {
-            solve(i + 1, j, maze, n, ans, path + "D", vis);
+        // Main function to find all possible paths in the maze
+        vector<string> ratInMaze(vector<vector<int>>& maze) {
+                int n = maze.size();
+                vector<string> ans; // Store all valid paths
+                vector<vector<int>> vis(n, vector<int>(n, 0)); // Visited array
+                
+                // Check if start and end positions are valid (value = 1)
+                if(maze[0][0] == 1 && maze[n - 1][n - 1] == 1) {
+                        solve(0, 0, maze, n, ans, "", vis);
+                }
+                
+                return ans;
         }
-        
-        if(j - 1 >= 0 && !vis[i][j - 1] && maze[i][j - 1] == 1) {
-            solve(i, j - 1, maze, n, ans, path + "L", vis);
-        }
-        
-        if(j + 1 < n && !vis[i][j + 1] && maze[i][j + 1] == 1) {
-            solve(i, j + 1, maze, n, ans, path + "R", vis);
-        }
-        
-        if(i - 1 >= 0 && !vis[i - 1][j] && maze[i - 1][j] == 1) {
-            solve(i - 1, j, maze, n, ans, path + "U", vis);
-        }
-        
-        vis[i][j] = 0;
-    }
-    
-    vector<string> ratInMaze(vector<vector<int>>& maze) {
-        int n = maze.size();
-        vector<string> ans;
-        vector<vector<int>> vis(n, vector<int>(n, 0));
-        
-        if(maze[0][0] == 1 && maze[n - 1][n - 1] == 1) {
-            solve(0, 0, maze, n, ans, "", vis);
-        }
-        
-        return ans;
-    }
 };
