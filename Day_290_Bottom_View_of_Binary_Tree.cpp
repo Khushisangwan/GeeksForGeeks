f3e4867061b11a0d3d06d06c36e576f61ca9b825
khushi <khushisangwan441@gmail.com>
2025-10-07 23:42:11 +0530

    fix: correct horizontal distance mapping in bottom view algorithm

--- Day_290_Bottom_View_of_Binary_Tree.cpp
+++ Day_290_Bottom_View_of_Binary_Tree.cpp
@@ -14,45 +14,58 @@ public:
 #include <bits/stdc++.h>
 using namespace std;
 class Solution {
-  public:
-    vector<int> bottomView(Node *root) {
-        // code here
-         unordered_map<Node*,int>omap;
-        map<int,vector<Node*>>mapForOrder;
-        
-        queue<Node*>q;
-        q.push(root);
-        omap[root]=0;
-        mapForOrder[0].push_back(root);
-        
-        while(!q.empty()) {
-            int counter=q.size();
-            for(int i=0;i<counter;i++) {
-                Node* vertex=q.front();
-                q.pop(); 
-                int location=omap[vertex];
-                if(vertex->left!=NULL) {
-                    q.push(vertex->left);
-                    omap[vertex->left]=location-1;
-                    mapForOrder[location-1].push_back(vertex->left);
+    public:
+        vector<int> bottomView(Node *root) {
+                // code here
+                
+                // Map to store horizontal distance for each node
+                unordered_map<Node*,int> omap;
+                
+                // Map to store nodes at each horizontal distance (ordered by distance)
+                map<int,vector<Node*>> mapForOrder;
+                
+                // Queue for level order traversal
+                queue<Node*> q;
+                q.push(root);
+                
+                // Root node is at horizontal distance 0
+                omap[root] = 0;
+                mapForOrder[0].push_back(root);
+                
+                // Level order traversal to assign horizontal distances
+                while(!q.empty()) {
+                        int counter = q.size();
+                        
+                        // Process all nodes at current level
+                        for(int i = 0; i < counter; i++) {
+                                Node* vertex = q.front();
+                                q.pop(); 
+                                
+                                int location = omap[vertex];
+                                
+                                // Left child gets horizontal distance - 1
+                                if(vertex->left != NULL) {
+                                        q.push(vertex->left);
+                                        omap[vertex->left] = location - 1;
+                                        mapForOrder[location - 1].push_back(vertex->left);
+                                }
+                                
+                                // Right child gets horizontal distance + 1
+                                if(vertex->right != NULL) {
+                                        q.push(vertex->right);
+                                        omap[vertex->right] = location + 1;
+                                        mapForOrder[location + 1].push_back(vertex->right);
+                                }
+                        }
                 }
                 
-                if(vertex->right!=NULL) {
-                    q.push(vertex->right);
-                    omap[vertex->right]=location+1;
-                    mapForOrder[location+1].push_back(vertex->right);
+                vector<int> ans;
+                
+                // For each horizontal distance, take the last node (bottom-most)
+                for(auto i : mapForOrder) {
+                        ans.push_back(i.second[i.second.size() - 1]->data);
                 }
-            }
-           
+                
+                return ans;
         }
-        
-        
-        vector<int>ans;
-        
-        for(auto i:mapForOrder) {
-            ans.push_back(i.second[i.second.size()-1]->data);
-        }
-        return ans;
-        
-    }
 };
