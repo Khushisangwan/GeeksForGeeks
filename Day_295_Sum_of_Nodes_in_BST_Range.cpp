a3965c8359acb85f9e2528c37853e90075bd1069
khushi <khushisangwan441@gmail.com>
2025-10-14 23:39:37 +0530

    style: enhance code readability by adding comments to node sum calculation in binary search tree

--- Day_295_Sum_of_Nodes_in_BST_Range.cpp
+++ Day_295_Sum_of_Nodes_in_BST_Range.cpp
@@ -1,14 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 class Solution {
-  public:
-    int nodeSum(Node* root, int l, int r) {
-        // code here
-        if(!root) return 0;
-        int ans = 0;
-        if(root -> data > l) ans += nodeSum(root -> left, l, r);
-        if(r > root -> data) ans += nodeSum(root -> right, l, r);
-        if(root -> data >= l && root -> data <= r) ans += root -> data;
-        return ans;
-    }
+    public:
+        int nodeSum(Node* root, int l, int r) {
+                // Base case: if node is null, return 0
+                if(!root) return 0;
+                
+                int ans = 0;
+                
+                // If current node's value is greater than lower bound,
+                // there might be valid nodes in left subtree
+                if(root -> data > l) ans += nodeSum(root -> left, l, r);
+                
+                // If current node's value is less than upper bound,
+                // there might be valid nodes in right subtree
+                if(r > root -> data) ans += nodeSum(root -> right, l, r);
+                
+                // If current node's value is within the range [l, r],
+                // include it in the sum
+                if(root -> data >= l && root -> data <= r) ans += root -> data;
+                
+                return ans;
+        }
 };
