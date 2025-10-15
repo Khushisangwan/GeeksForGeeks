86edf4b2aa6062edd1bc5d33b8987674f36054d5
khushi <khushisangwan441@gmail.com>
2025-10-15 23:11:36 +0530

    style: improve code formatting and readability in kthSmallest function

--- Day_296_k_th_Smallest_in_BST.cpp
+++ Day_296_k_th_Smallest_in_BST.cpp
@@ -14,20 +14,31 @@ class Node {
 #include <bits/stdc++.h>
 using namespace std;
 class Solution {
-  public:
-    int kthSmallest(Node *root, int &k) {
-       if(root==NULL)
-        return -1;
-        int left=kthSmallest(root->left,k);
-        if(left!=-1)
-        {
-        return left;
+    public:
+        int kthSmallest(Node *root, int &k) {
+             // Base case: if node is NULL, return -1
+             if(root==NULL)
+                return -1;
+                
+                // Recursively search in left subtree (smaller elements)
+                int left=kthSmallest(root->left,k);
+                
+                // If kth smallest found in left subtree, return it
+                if(left!=-1)
+                {
+                return left;
+                }
+                
+                // Process current node - decrement k
+                k--;
+                
+                // If this is the kth node, return its data
+                if(k==0)
+                {
+                return root->data;
+                }
+                
+             // Search in right subtree (larger elements)
+             return kthSmallest(root->right,k); 
         }
-        k--;
-        if(k==0)
-        {
-        return root->data;
-        }
-       return kthSmallest(root->right,k); 
-    }
 };
