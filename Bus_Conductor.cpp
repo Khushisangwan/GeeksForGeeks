1502c5d3d7c42d1c2f2f4d99e497e6c8a623c51d
khushi <khushisangwan441@gmail.com>
2025-12-19 23:16:17 +0530

    style: enhance code readability by adding comments and improving formatting in findMoves function

--- Bus_Conductor.cpp
+++ Bus_Conductor.cpp
@@ -1,15 +1,19 @@
+
 class Solution {
-  public:
-    int findMoves(vector<int>& chairs, vector<int>& passengers) {
-        sort(chairs.begin(),chairs.end());
-        sort(passengers.begin(),passengers.end());
-        
-        int res = 0;
-        
-        for(int i=0;i<chairs.size();i++){
-            res += abs(chairs[i]-passengers[i]);
+    public:
+        int findMoves(vector<int>& chairs, vector<int>& passengers) {
+                // Sort chairs in ascending order
+                sort(chairs.begin(), chairs.end());
+                // Sort passengers in ascending order
+                sort(passengers.begin(), passengers.end());
+                
+                int res = 0;
+                
+                // Calculate total moves needed by matching each passenger to nearest chair
+                for(int i = 0; i < chairs.size(); i++){
+                        res += abs(chairs[i] - passengers[i]);
+                }
+                
+                return res;
         }
-        
-        return res;
-    }
 };
