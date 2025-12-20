fd9d21662d85438f3b7d1534ba90ef77f632ebd1
khushi <khushisangwan441@gmail.com>
2025-12-20 23:11:29 +0530

    style: move includes and namespace declaration to the top of the file for better organization

---
+++ Search_insert_position_of_K_in_a_sorted_array.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    int searchInsertK(vector<int> &arr, int k) {
+        int start = 0, end = arr.size()-1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] < k) { 
+                start = mid + 1;
+            } else { 
+                end = mid - 1; 
+            }
+        }
+        return start;
+    }
+};
