a62fce70be6e81cb5d4ac47a97880466ccf2bc4b
khushi <khushisangwan441@gmail.com>
2025-12-23 15:36:30 +0530

    style: enhance comments for clarity and organization in cntInRange function

--- Elements_in_range.cpp
+++ Elements_in_range.cpp
@@ -1,23 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 class Solution {
-  public:
-    vector<int> cntInRange(vector<int> &arr, vector<vector<int>> &queries) {
-        // code here
-         ios_base::sync_with_stdio(false);
-        vector <int> res;
-        sort(arr.begin(), arr.end());
-        
-        for(auto query : queries){
-            int a = query[0];
-            int b = query[1];
-            
-            auto low = lower_bound(arr.begin(), arr.end(), a);
-            auto high = upper_bound(arr.begin(), arr.end(), b);
-            int cnt = high - low;
-            res.push_back(cnt);
+    public:
+        vector<int> cntInRange(vector<int> &arr, vector<vector<int>> &queries) {
+                // Optimize I/O for faster execution
+                ios_base::sync_with_stdio(false);
+                
+                vector<int> res;
+                
+                // Sort the array to enable binary search
+                sort(arr.begin(), arr.end());
+                
+                // Process each query
+                for(auto query : queries){
+                        int a = query[0];  // Lower bound of range
+                        int b = query[1];  // Upper bound of range
+                        
+                        // Find first element >= a
+                        auto low = lower_bound(arr.begin(), arr.end(), a);
+                        
+                        // Find first element > b
+                        auto high = upper_bound(arr.begin(), arr.end(), b);
+                        
+                        // Count elements in range [a, b]
+                        int cnt = high - low;
+                        res.push_back(cnt);
+                }
+                
+                return res;
         }
-        
-        return res;
-    }
 };
