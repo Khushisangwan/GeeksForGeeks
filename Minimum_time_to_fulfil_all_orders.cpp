9866c5ada701e174515fb6b640388c1d541a2dd6
khushi <khushisangwan441@gmail.com>
2025-12-28 22:04:44 +0530

    style: enhance code readability by improving comments and formatting in minTime method

--- Minimum_time_to_fulfil_all_orders.cpp
+++ Minimum_time_to_fulfil_all_orders.cpp
@@ -1,21 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 class Solution {
-  public:
-    using p = pair<int,pair<int,int>>;
-    int minTime(vector<int>& ranks, int n) {
-        priority_queue<p,vector<p>,greater<p>> pq;
-        int m = ranks.size(); // number of chefs
-        for(int i=0;i<m;i++){
-            pq.push({ranks[i],{2*ranks[i],i}});
+    public:
+        // Pair type: {current_time, {time_increment, chef_index}}
+        using p = pair<int,pair<int,int>>;
+        
+        int minTime(vector<int>& ranks, int n) {
+                // Min heap to track chefs by their current completion time
+                priority_queue<p,vector<p>,greater<p>> pq;
+                int m = ranks.size(); // number of chefs
+                
+                // Initialize heap with each chef's rank and initial time increment
+                for(int i=0;i<m;i++){
+                        pq.push({ranks[i],{2*ranks[i],i}});
+                }
+                
+                int ans = 0;
+                // Process n orders
+                while(n--){
+                        // Get chef with minimum current completion time
+                        p temp = pq.top();
+                        pq.pop();
+                        
+                        // Update answer with maximum time seen so far
+                        ans=max(ans,temp.first);
+                        
+                        // Push chef back with updated completion time
+                        pq.push({temp.first+temp.second.first,{temp.second.first+ranks[temp.second.second],temp.second.second}});
+                }
+                return ans;
         }
-        int ans = 0;
-        while(n--){
-            p temp = pq.top();
-            pq.pop();
-            ans=max(ans,temp.first);
-            pq.push({temp.first+temp.second.first,{temp.second.first+ranks[temp.second.second],temp.second.second}});
-        }
-        return ans;
-    }
 };
