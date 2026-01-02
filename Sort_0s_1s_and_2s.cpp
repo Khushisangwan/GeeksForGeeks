b37c191d7c977c961c7c3f2a55ce1abca2bd6a53
khushi <khushisangwan441@gmail.com>
2026-01-02 23:43:32 +0530

    style: improve formatting and add comments for clarity in sort012 method

--- Sort_0s_1s_and_2s.cpp
+++ Sort_0s_1s_and_2s.cpp
@@ -1,31 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 class Solution {
-  public:
-    void sort012(vector<int>& arr) {
-        int zero=0;
-        int one=0;
-        int two=0;
-        
-        for(int i=0;i<arr.size();i++){
-            if(arr[i]==0)zero++;
-            else if(arr[i]==1)one++;
-            else if(arr[i]==2)two++;
-        } 
-        for(int i=0;i<arr.size();i++){
-            if(zero){
-                arr[i]=0;
-                zero--;
-            }
-            else if(one){
-                arr[i]=1;
-                one--;
-            }
-            else if(two){
-                arr[i]=2;
-                two--;
-            }
+    public:
+        void sort012(vector<int>& arr) {
+                // Count occurrences of 0, 1, and 2
+                int zero = 0;
+                int one = 0;
+                int two = 0;
+                
+                for(int i = 0; i < arr.size(); i++){
+                        if(arr[i] == 0) zero++;
+                        else if(arr[i] == 1) one++;
+                        else if(arr[i] == 2) two++;
+                } 
+                
+                // Reconstruct array with sorted values
+                for(int i = 0; i < arr.size(); i++){
+                        if(zero){
+                                arr[i] = 0;
+                                zero--;
+                        }
+                        else if(one){
+                                arr[i] = 1;
+                                one--;
+                        }
+                        else if(two){
+                                arr[i] = 2;
+                                two--;
+                        }
+                }
         }
-        
-    }
 };
