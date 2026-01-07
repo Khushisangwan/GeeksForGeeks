2145977368d6473bbed6707dda7a926d9b32c993
khushi <khushisangwan441@gmail.com>
2026-01-07 23:03:24 +0530

    style: improve code formatting and indentation for better readability

--- Count_distinct_elements_in_every_window.cpp
+++ Count_distinct_elements_in_every_window.cpp
@@ -1,28 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
 class Solution {
-  public:
-    vector<int> countDistinct(vector<int> &arr, int k) {
-      vector<int> result;
-    unordered_map<int, int> freqMap;
+    public:
+        vector<int> countDistinct(vector<int> &arr, int k) {
+            vector<int> result;
+        unordered_map<int, int> freqMap;
 
-    for (int i = 0; i < k; ++i) {
-        freqMap[arr[i]]++;
-    }
-
-    result.push_back(freqMap.size());
-
-    for (int i = k; i < arr.size(); ++i) {
-        freqMap[arr[i - k]]--;
-        if (freqMap[arr[i - k]] == 0) {
-            freqMap.erase(arr[i - k]);
+        // Initialize the frequency map with first k elements
+        for (int i = 0; i < k; ++i) {
+                freqMap[arr[i]]++;
         }
 
-        freqMap[arr[i]]++;
-
+        // Add count of distinct elements in first window
         result.push_back(freqMap.size());
-    }
 
-    return result;
-    }
+        // Slide the window through the rest of the array
+        for (int i = k; i < arr.size(); ++i) {
+                // Remove the leftmost element of previous window
+                freqMap[arr[i - k]]--;
+                if (freqMap[arr[i - k]] == 0) {
+                        freqMap.erase(arr[i - k]);
+                }
+
+                // Add the new element to the window
+                freqMap[arr[i]]++;
+
+                // Store the count of distinct elements in current window
+                result.push_back(freqMap.size());
+        }
+
+        return result;
+        }
 };
