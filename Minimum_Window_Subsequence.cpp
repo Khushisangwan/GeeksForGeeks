d11ea2cd1ed34778ae88918fa76f1f54bed39544
khushi <khushisangwan441@gmail.com>
2026-01-11 19:25:10 +0530

    style: add comments for clarity in minWindow method

--- Minimum_Window_Subsequence.cpp
+++ Minimum_Window_Subsequence.cpp
@@ -9,24 +9,30 @@ public:
         string ans = "";
         int len = INT_MAX;
 
+        // Forward pass: find a window containing s2 as a subsequence
         while (i < n) {
+            // Match characters of s2 in s1
             if (s1[i] == s2[j]) j++;
 
+            // When entire s2 is matched
             if (j == m) {
                 int end = i;
                 j = m - 1;
 
+                // Backward pass: shrink the window from left
                 while (i >= 0 && j >= 0) {
                     if (s1[i] == s2[j]) j--;
                     i--;
                 }
                 i++;
 
+                // Update answer if current window is smaller
                 if (end - i + 1 < len) {
                     len = end - i + 1;
                     ans = s1.substr(i, len);
                 }
 
+                // Reset j for next iteration
                 j = 0;
             }
             i++;
