e70cc56327320d5274842ee1fec36f85044911cd
khushi <khushisangwan441@gmail.com>
2026-01-15 19:22:50 +0530

    style: enhance code readability in minCandy method with consistent formatting and comments

--- Candy.cpp
+++ Candy.cpp
@@ -1,32 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 class Solution {
-  public:
-    int minCandy(vector<int> &arr) {
-        int n=arr.size();
-        int sum=1;
-        int i=1;
-        while(i<n){
-        if(arr[i]==arr[i-1]){
-            sum+=1;
-            i++;
+    public:
+        int minCandy(vector<int> &arr) {
+                int n = arr.size();
+                int sum = 1;  // Start with 1 candy for the first element
+                int i = 1;
+                
+                while(i < n) {
+                        // If current element equals previous, give 1 candy
+                        if(arr[i] == arr[i-1]) {
+                                sum += 1;
+                                i++;
+                        }
+                        
+                        // Count increasing sequence and add candies
+                        int peak = 1;
+                        while(i < n && arr[i] > arr[i-1]) {
+                                peak += 1;
+                                sum += peak;
+                                i++;
+                        }
+                        
+                        // Count decreasing sequence and add candies
+                        int down = 1;
+                        while(i < n && arr[i] < arr[i-1]) {
+                                sum += down;
+                                down++;
+                                i++;
+                        }
+                        
+                        // If descent is longer than ascent, add extra candies at the peak
+                        if(down > peak) {
+                                sum += (down - peak);
+                        }
+                }
+                return sum;
         }
-        int peak=1;
-        while(i<n&&arr[i]>arr[i-1]){
-        peak+=1;
-        sum+=peak;
-        i++;
-        }
-        int down=1;
-        while(i<n&&arr[i]<arr[i-1]){
-        sum+=down;
-        down++;
-        i++;
-        }
-        if(down>peak){
-            sum+=(down-peak);
-        }
-        }
-        return sum;
-    }
 };
