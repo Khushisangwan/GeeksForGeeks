3df50579fe16aadd8cd4d8d26802b4ad6ba6627e
khushi <khushisangwan441@gmail.com>
2026-01-31 13:34:52 +0530

    style: improve code formatting and comments for clarity in Implement_k_Queues_in_a_Single_Array.cpp

--- Implement_k_Queues_in_a_Single_Array.cpp
+++ Implement_k_Queues_in_a_Single_Array.cpp
@@ -1,32 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+// Class to implement k queues in a single array
 class kQueues {
-  public:
-  int cnt, nn;
-  vector<queue<int>>vq;
-    kQueues(int n, int k) {
-        cnt=0; nn=n;
-        vq.resize(k);
-    }
-
-    void enqueue(int x, int i) {
-        vq[i].push(x);
-        cnt++;
-    }
-
-    int dequeue(int i) {
-        if(vq[i].size()){
-            int xx=vq[i].front(); vq[i].pop();
-            cnt--; return xx;
+    public:
+    int cnt, nn;  // cnt: current number of elements, nn: total array size
+    vector<queue<int>>vq;  // Vector of queues, one for each queue
+    
+        // Constructor: initialize k queues with max size n
+        kQueues(int n, int k) {
+                cnt=0; nn=n;
+                vq.resize(k);
         }
-        return -1;
-    }
 
-    bool isEmpty(int i) {
-       return !vq[i].size();
-    }
+        // Enqueue element x into queue i
+        void enqueue(int x, int i) {
+                vq[i].push(x);
+                cnt++;
+        }
 
-    bool isFull() {
-        return cnt==nn;
-    }
+        // Dequeue element from queue i
+        int dequeue(int i) {
+                if(vq[i].size()){
+                        int xx=vq[i].front(); 
+                        vq[i].pop();
+                        cnt--; 
+                        return xx;
+                }
+                return -1;  // Return -1 if queue is empty
+        }
+
+        // Check if queue i is empty
+        bool isEmpty(int i) {
+             return !vq[i].size();
+        }
+
+        // Check if all queues are full
+        bool isFull() {
+                return cnt==nn;
+        }
 };
