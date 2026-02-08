74614368cb3c27b2a1202ec02658b0e82f38d55c
khushi <khushisangwan441@gmail.com>
2026-02-08 19:32:44 +0530

    style: improve code formatting and comments for clarity in Maximum_Product_Subarray.cpp

--- Maximum_Product_Subarray.cpp
+++ Maximum_Product_Subarray.cpp
@@ -1,19 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 class Solution {
-  public:
-    int maxProduct(vector<int> &arr) {
-        int n = arr.size() , ans = INT_MIN;
-        int pref = 1 , suff = 1;
-        for(int i = 0; i < n; i++){
-            pref *= arr[i];
-            suff *= arr[n - i - 1];
-            ans = max({ans , pref , suff});
-            if(pref == 0)
-                pref = 1;
-            if(suff == 0)
-                suff = 1;
+    public:
+        int maxProduct(vector<int> &arr) {
+                int n = arr.size() , ans = INT_MIN;
+                int pref = 1 , suff = 1;
+                
+                // Iterate through the array
+                for(int i = 0; i < n; i++){
+                        // Multiply prefix (left to right)
+                        pref *= arr[i];
+                        // Multiply suffix (right to left)
+                        suff *= arr[n - i - 1];
+                        // Update maximum product found so far
+                        ans = max({ans , pref , suff});
+                        
+                        // Reset prefix to 1 if it becomes 0
+                        if(pref == 0)
+                                pref = 1;
+                        // Reset suffix to 1 if it becomes 0
+                        if(suff == 0)
+                                suff = 1;
+                }
+                return ans;
         }
-        return ans;
-    }
 };
